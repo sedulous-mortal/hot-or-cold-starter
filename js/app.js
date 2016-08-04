418a30ebcc9ea277eb6c18d813e150e53b5a6b7b
Alison Stuart <alisonnicolestuart@gmail.com>
2016-08-03 23:12:57 -0400

    Minor changes to try to follow step 3, which says to break down the steps and tackle small chunks at a time.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
+'use strict';
+
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,6 +14,17 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+//make new function called newGame()
+	//clear the entire div 
+	//set rand(secret_num)
+	//when value entered,
+	//preventDefault();
+	//assign that value to a new variable called var userGuess
+	//turn userGuess string into a number
+	userGuess = +userGuess;
+		//make function called compareGuess() 
+			//that evaluates whether secret_num === userGuess
+	compareGuess();
 });
 
 
