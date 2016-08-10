12988c3c14a31ff05c0b63d03cd0cc2323001c00
Alison Stuart <alisonnicolestuart@gmail.com>
2016-08-10 10:48:49 -0400

    Some basic changes from the original pseudocode, trying to flesh it out.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,42 @@
-
 'use strict';
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+$(document).ready(function () {
 
-  	});
+    /*--- Display information modal box ---*/
+    $(".what").click(function () {
+        $(".overlay").fadeIn(1000);
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+    });
 
-//make new function called newGame()
-	//clear the entire div 
-	//set rand(secret_num)
-	//when value entered,
-	//preventDefault();
-	//assign that value to a new variable called var userGuess
-	//turn userGuess string into a number
-	userGuess = +userGuess;
-		//make function called compareGuess() 
-			//that evaluates whether secret_num === userGuess
-	compareGuess();
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function () {
+        $(".overlay").fadeOut(1000);
+    });
+
+    //make new function called newGame
+    function newGame() {
+        document.getElementById("userGuess").innerHTML = "";
+
+        function newSecret() {
+            var secret_num = Math.floor((Math.random() * 100) + 1);
+        }
+        newSecret();
+    }
+    //temporary answer-giver to test code
+    document.getElementById("feedback").innerHTML = "The secret number is " + secret_num + "!";
+
+    //handler for submit form:
+    preventDefault();
+    var userGuess = document.getElementById("userGuess").innerHTML;
+    //turn userGuess string into a number
+    userGuess = +userGuess;
+    compareGuess() {
+        secret_num === userGuess;
+        return;
+    }
+    if ('compareGuess()') {
+        $('#feedback').append('You guessed the right number! Click \"New Game\" to play again.')
+    }
+
+
 });
-
-
